f5450b7d1a806077ee776f51b6ddc52e7da75e9e
Sam Flavin <seamus@Sams-Laptop.local>
2025-04-04 12:58:20 +0800

    added middleware to make reqs from FE

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routes import campaigns, creatives, creative_groups
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Allow the frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(campaigns.router, prefix="/campaigns", tags=["Campaigns"])
 app.include_router(creatives.router, prefix="/creatives", tags=["Creatives"])
